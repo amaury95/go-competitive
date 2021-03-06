8809a8af3a2badc9c9077874609ddeb0cd7c8fd3
Amaury Diaz <amauryuh@gmail.com>
2021-03-06 00:18:55 -0500

    Add mergesort and quicksort implementations.

--- sorting/merge.go
+++ sorting/merge.go
@@ -2,5 +2,34 @@ package sorting
 
 // MergeSort returns a sorted array by using mergesort
 func MergeSort(arr []int) []int {
-	return arr
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	middle := len(arr) / 2
+
+	return merge(MergeSort(arr[:middle]), MergeSort(arr[middle:]))
+}
+
+func merge(left, right []int) []int {
+	l, r := 0, 0
+
+	result := make([]int, len(left)+len(right))
+
+	for i := 0; i < len(result); i++ {
+		if l == len(left) {
+			result[i] = right[r]
+			r++
+		} else if r == len(right) {
+			result[i] = left[l]
+			l++
+		} else if left[l] <= right[r] {
+			result[i] = left[l]
+			l++
+		} else {
+			result[i] = right[r]
+			r++
+		}
+	}
+	return result
 }
--- sorting/quick.go
+++ sorting/quick.go
@@ -2,5 +2,29 @@ package sorting
 
 // QuickSort https://medium.com/@rgalus/sorting-algorithms-quick-sort-implementation-in-go-9ebfd91fe95f
 func QuickSort(arr []int) []int {
-	return arr
+	result := append([]int{}, arr...)
+	sort(result, 0, len(arr)-1)
+	return result
+}
+
+func sort(arr []int, start, end int) {
+	if end-start < 1 {
+		return
+	}
+
+	pivot := arr[end]
+	split := start
+
+	for i := start; i < end; i++ {
+		if arr[i] < pivot {
+			arr[i], arr[split] = arr[split], arr[i]
+			split++
+		}
+	}
+
+	arr[end] = arr[split]
+	arr[split] = pivot
+
+	sort(arr, start, split-1)
+	sort(arr, split+1, end)
 }
